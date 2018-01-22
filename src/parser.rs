fa3f42b5e155c63010030c4d3a35381dcc48739e
elpinal <elpinal@users.noreply.github.com>
2018-01-22 23:55:11 +0900

    Write the return type for Parser::read

--- src/parser.rs
+++ src/parser.rs
@@ -25,7 +25,7 @@ impl<R: Sized + Read> Parser<R> {
         self.r.next()
     }
 
-    fn read(&mut self) {
+    fn read(&mut self) -> Result<u8, io::Error> {
         match self.peek() {
             Some(r) => {
                 match *r {
